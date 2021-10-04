f4e0457feb709455f2f7f3197e5d96429295ce43
uukkii <i.am.newbie@hotmail.com>
2021-10-04 20:17:56 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Main {
+
+    private static final HashSet<Students> studentsBase = new HashSet<>();
+    private static final Scanner console = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Добро пожаловать!");
+        while (true) {
+            System.out.println("Введите информацию о суденте в следующем формате: ФИО, № группы, ID студенческого билета.\n" +
+                    "Для завершения введите 'end'");
+
+            String input = console.nextLine();
+            if ("end".equals(input)) {
+                break;
+            }
+            String[] arrayInput = input.split(", ");
+            String name = arrayInput[0], group = arrayInput[1], studentId = arrayInput[2];
+            addStudent(name, group, studentId);
+        }
+        showStudentsBase();
+    }
+
+    public static void addStudent(String name, String group, String studentId) {
+        Students newStudent = new Students(name, group, studentId);
+        if (studentsBase.contains(newStudent)) {
+            System.out.println("Введенный номер студенческого билета уже присвоен другому студенту!\n");
+        } else studentsBase.add(newStudent);
+    }
+
+    public static void showStudentsBase() {
+        System.out.println("Список студентов:");
+        for (Students newStudent : studentsBase) {
+            System.out.println(" - " + newStudent);
+        }
+    }
+}
---
+++ src/Students.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class Students {
+
+    private String name;
+    private String group;
+    private String studentId;
+
+    public Students(String name, String group, String  studentId) {
+        this.name = name;
+        this.group = group;
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    @Override
+    public String toString(){
+        return name + ", " + group + ", " + studentId + ".";
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || !object.getClass().equals(Students.class))
+            return false;
+        Students altStudent = (Students) object;
+        return this.studentId != altStudent.studentId;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(studentId);
+    }
+}
